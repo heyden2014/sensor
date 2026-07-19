8d6dccfe364be11fc8e7baa178ba66df3d525051
Juan Antonio Paulino <43677954+heyden2014@users.noreply.github.com>
2026-07-19 19:09:19 -0400

    Update sensor data fetching URL

--- sensor.js
+++ sensor.js
@@ -17,7 +17,7 @@ function enviarDatos() {
    //console.log(datos)
    
          
-    fetch("https://scintillating-lokum-34e1f4.netlify.app/sensor/recibir.php", {method: 'POST', body: JSON.stringify(datos)})
+    fetch("sensor-gilt-seven.vercel.app/sensor/recibir.php", {method: 'POST', body: JSON.stringify(datos)})
       
     .then(response=>response.json())
         .then((data)=>{
