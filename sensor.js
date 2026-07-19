8edf0e2ec29d0c8460c32b4847f5fbc60835046c
Juan Antonio Paulino <43677954+heyden2014@users.noreply.github.com>
2026-07-19 19:17:58 -0400

    Fix fetch URL in enviarDatos function

--- sensor.js
+++ sensor.js
@@ -17,7 +17,7 @@ function enviarDatos() {
    //console.log(datos)
    
          
-    fetch("sensor-gilt-seven.vercel.app/sensor/recibir.php", {method: 'POST', body: JSON.stringify(datos)})
+    fetch("sensor-gilt-seven.vercel.app/recibir.php", {method: 'POST', body: JSON.stringify(datos)})
       
     .then(response=>response.json())
         .then((data)=>{
