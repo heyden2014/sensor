58472fc5fe996e093fc702226ea5950c13638c28
Juan Antonio Paulino <43677954+heyden2014@users.noreply.github.com>
2026-08-04 18:51:50 -0400

    Remove CORS headers from recibir.php

--- recibir.php
+++ recibir.php
@@ -1,9 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET,POST, OPTIONS");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 
 $data = json_decode(file_get_contents("php://input"));
 
